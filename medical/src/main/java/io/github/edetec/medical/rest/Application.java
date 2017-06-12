ef131fd1e30387ef63a20cded4a0bbf34d6756f4
Eder Soares <edetec@gmail.com>
2017-06-12 17:43:01 -0300

    Hibenate configurations

--- medical/src/main/java/io/github/edetec/medical/rest/Application.java
+++ medical/src/main/java/io/github/edetec/medical/rest/Application.java
@@ -4,9 +4,12 @@ import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import io.github.edetec.medical.util.JpaUtil;
+
 @ApplicationPath("api")
 public class Application extends ResourceConfig {
 	public Application() {
+		JpaUtil.getSessionFactory();
 		packages("io.github.edetec.medical.service");
 	}
 }
